33842f7e3abe4ec470f3a9deb1d7d774eac2a1e6
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2023-12-11 20:08:02 +0200

    fix git

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Filter } from 'components/Filter/Filter';
+import { Div } from 'components/ContactForm/ContactForm.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Div>
+      <h1>Phonebook</h1>
+      <ContactForm/>
+      <h2>Contacts</h2>
+      <Filter></Filter>
+      <ContactList></ContactList>
+    </Div>
   );
 };
